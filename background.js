876ed6c6729c71a00c87487099f2cffdbd4da71a
Lune <lunasun918@gmail.com>
2022-02-15 10:21:55 -0500

    adding background + permissions

---
+++ background.js
@@ -0,0 +1,8 @@
+// background.js
+
+let color = '#679179';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
