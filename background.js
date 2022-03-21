c8a631ef1305810576d428f246a9f98e95e9d303
Lune <lunasun918@gmail.com>
2022-03-21 09:08:15 -0400

    error 2.0

--- background.js
+++ background.js
@@ -7,11 +7,3 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
 
-/* let button = document.createElement("button");
-button.innerHTML = "Click Me";
-btn.onclick = function () {
-  location.href = "to-do.html";
-};
-btn.type = "submit";
-btn.name = "to-do";
-document.body.appendChild(button); */
