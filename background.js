4937dce170fa8a95e24d430b617271245487f51b
Lune <lunasun918@gmail.com>
2022-03-21 09:06:28 -0400

    button

--- background.js
+++ background.js
@@ -6,3 +6,12 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+let button = document.createElement("button");
+button.innerHTML = "Click Me";
+btn.onclick = function () {
+  location.href = "to-do.html";
+};
+btn.type = "submit";
+btn.name = "to-do";
+document.body.appendChild(button);
