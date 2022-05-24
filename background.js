ba6842cce026473bbf781784f2815a093844a34a
Lune <lunasun918@gmail.com>
2022-05-24 10:58:01 -0400

    the size is finally fixed

--- background.js
+++ background.js
@@ -1,17 +1,6 @@
-// background.js
-
-let color = '#F7F7F7';
+let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgrey', `color: ${color}`);
+  console.log('Default background color set to %cgreen', `color: ${color}`);
 });
-
-/* let button = document.createElement("button");
-button.innerHTML = "Click Me";
-btn.onclick = function () {
-  location.href = "to-do.html";
-};
-btn.type = "submit";
-btn.name = "to-do";
-document.body.appendChild(button); */
